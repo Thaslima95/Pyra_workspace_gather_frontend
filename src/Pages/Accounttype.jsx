63496857da2e29238c7cb096ca32c0fc6a09e9ff
manojsundar18 <manoj@pyramidions.in>
2023-10-26 10:25:01 +0530

    firstcommit

---
+++ src/Pages/Accounttype.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Accounttype() {
+  return <div>Accounttype</div>;
+}
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,49 @@
+// import React from "react";
+
+// const Home = () => {
+//   return (
+//     <div className="home">
+//       <header>
+//         <h1>Pyramidon</h1>
+//       </header>
+//       <main>
+//         {" "}
+//         <h1>Experiencing new way of Interacting</h1>
+//         <h4>
+//           Create your own virtual world and make your interactions more human
+//         </h4>{" "}
+//       </main>
+//       <footer>&copy; {new Date().getFullYear} My Homepage</footer>
+//     </div>
+//   );
+// };
+
+// export default Home;
 import React from "react";
 
-export default function Home() {
-  return <div>Home</div>;
-}
+const Home = () => {
+  return (
+    <div className="home">
+      <header>
+        <h1>Pyramidon</h1>
+        <div className="header-buttons">
+          <button>Login</button>
+          <button>Register Free</button>
+        </div>
+      </header>
+      <main>
+        <h1>Experiencing a new way of Interacting</h1>
+        <h4>
+          Create your own virtual world and make your interactions more human
+        </h4>
+        <div className="main-buttons">
+          <button>Refister Free</button>
+          <button>Try Now</button>
+        </div>
+      </main>
+      <footer>&copy; {new Date().getFullYear()} My Homepage</footer>
+    </div>
+  );
+};
+
+export default Home;
